b8724010248db748d3b143adf7e1fdd300d03046
Avishek-kr <ak251997@gmail.com>
2021-02-14 21:25:04 +0530

    completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -7,28 +7,32 @@ import ExpandMoreIcon  from '@material-ui/icons/ExpandMore'
 const Item = ({title,desc,backgroundImg,leftBtnTxt,leftBtnLink,rightBtnTxt,rightBtnLink,twoButtons,first}) => {
  
     return (
-        <div className='item' style={{
+        <div className="item" style={{
                 backgroundImage: `url(${backgroundImg})`
         }}>
-            <div className='item_container'>
-                <div className='item_text'>
+            <div className="item_container">
+                <div className="item_text">
                     <p>{title}</p>
-                    <div className='item_textDesc'>
+                    <div className="item_textDesc">
                         <p>{desc}</p>
                     </div>
                 </div>
-                <div className='item_lowerThird'>
-                    <div className='item_buttons'>
+                <div className="item_lowerThird">
+                    <div className="item_buttons">
                         <Button imp='primary' text={leftBtnTxt} link={leftBtnLink} />
+                       
                         {twoButtons && (
                             <Button imp='secondary' text={rightBtnTxt} link={rightBtnLink} />
                         )}
                     </div>
+                    <div className="arrow">
                     {first && (
                         <div className="item_expand">
                             <ExpandMoreIcon />
                         </div>
                     )}
+                    </div>
+                   
                 </div>
             </div>
             
